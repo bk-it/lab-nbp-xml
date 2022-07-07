c58047d7c0744961dfffffa3d8e546f21ce3a845
vm <vm@DESKTOP-66HFB28>
2022-07-07 15:51:05 +0200

    Add project files.

---
+++ lab-nbp-xml/CurrencyXML.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace lab_nbp_xml
+{
+    public static class CurrencyXML
+    {
+        public static void Start()
+        {
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://www.nbp.pl/kursy/xml/lastC.xml");
+            StreamReader reader = new StreamReader(stream);
+            string content = reader.ReadToEnd();
+            XDocument doc = XDocument.Parse(content);
+            doc.Declaration = new XDeclaration("1.0", "utf-8", null);
+
+
+            var result = doc.Descendants("kod_waluty")
+                    .Where(el => el.Value == "EUR")
+                    .First()
+                    .Parent;
+
+
+
+            var result2 = result.Element("nazwa_waluty").Value;
+
+            Console.WriteLine(result2);
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ lab-nbp-xml/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static lab_nbp_xml.CurrencyXML;
+
+namespace lab_nbp_xml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Start();
+            }
+            catch
+            {
+                Console.WriteLine("błąd");
+            }
+
+
+
+        }
+    }
+}
